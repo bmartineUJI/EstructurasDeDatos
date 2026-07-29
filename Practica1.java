6f6fe389060d2beb0c274d0d07c0cb559415000e
bmartineUJI <bmartine@uji.es>
2026-07-29 12:45:24 +0200

    Add files via upload

---
+++ Practica1.java
@@ -0,0 +1,33 @@
+package practica1;
+
+import java.util.*;
+
+public class Practica1 {
+
+    //EJERCICIO 1
+    public static Set<Integer> multiplos (Iterator<Integer> it) {
+        //TODO
+        return null;
+    }
+
+    //EJERCICIO2
+    public static void separate (Set<Integer> cuadrados, Set<Integer> noCuadrados)  {
+        //TODO
+
+    }
+
+    //EJERCICIO 3
+    public static<T> Collection<Set<T>> divideInSets (Iterator<T> it) {
+        //TODO
+        return null;
+    }
+
+    //EJERCICIO 4
+    public static<T> Collection<Set<T>> coverageSet2 (Set<T> u,ArrayList<Set<T>> col) {
+        //TODO
+        return null;
+    }
+
+
+
+}
